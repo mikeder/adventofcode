f88c56c00592aa625f207ec32481c87816a75124
mikeder <mikedernet@gmail.com>
2018-12-03 22:14:31 -0500

    tweak tests

--- 2018/shared/lib_test.go
+++ 2018/shared/lib_test.go
@@ -1,13 +1,12 @@
 package shared
 
 import (
-	"adventofcode/2018/shared"
 	"testing"
 )
 
 func TestGetInputPathSingleDigit(t *testing.T) {
 	input := 1
-	result := shared.GetInputPath(input)
+	result := GetInputPath(input)
 	if result != "./day_01/input.txt" {
 		t.Error("Wrong result")
 	}
@@ -15,7 +14,7 @@ func TestGetInputPathSingleDigit(t *testing.T) {
 
 func TestGetInputPathDoubleDigit(t *testing.T) {
 	input := 10
-	result := shared.GetInputPath(input)
+	result := GetInputPath(input)
 	if result != "./day_10/input.txt" {
 		t.Error("Wrong result")
 	}
@@ -23,7 +22,7 @@ func TestGetInputPathDoubleDigit(t *testing.T) {
 
 func TestGetInputPathDoubleDigit2(t *testing.T) {
 	input := 20
-	result := shared.GetInputPath(input)
+	result := GetInputPath(input)
 	if result != "./day_20/input.txt" {
 		t.Error("Wrong result")
 	}
